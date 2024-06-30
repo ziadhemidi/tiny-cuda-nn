6252a696c50b4e15a4fa295e7107abbf90d7e216
Ziad Al-Haj Hemidi <ziad.alhajhemidi@uni-luebeck.de>
2024-06-30 19:51:11 +0200

    refactor: Update activation function calculation in common_device.h

--- include/tiny-cuda-nn/common_device.h
+++ include/tiny-cuda-nn/common_device.h
@@ -264,7 +264,7 @@ __host__ __device__ void warp_activation_backward(Activation activation, const f
 			// assert(false); // Commented out due to isolated strange side-effects on Windows
 			TCNN_PRAGMA_UNROLL
 			for (int t=0; t < result.num_elements; t++) {
-				result.x[t] = (T)((float)frag.x[t] * 30.0f) * (T)(cosf((float)forward_frag_in.x[t] * 30.0f));
+				result.x[t] = (T)((float)frag.x[t] * 30.0f) * (T)(cosf((float)forward_frag.x[t] * 30.0f));
 			}
 			return;
 		case Activation::Sigmoid:
